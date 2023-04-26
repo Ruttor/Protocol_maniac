3d8afec641def969152a5552351aba10b9e8c2cd
Ruttor <fabianfelden.ff@gmail.com>
2023-04-26 20:25:42 +0200

    added test for errorcalc. included float and int output in LaTeX code.

--- errorcalc.py
+++ errorcalc.py
@@ -1,9 +1,8 @@
 import inspect
 import sympy as sym
-import numpy as np
 
 
-class SymbolicDerivatives:
+class error:
     def __init__(self, func):
         # Get the arguments of the function
         arg_names = inspect.getfullargspec(func).args
@@ -33,14 +32,14 @@ class SymbolicDerivatives:
             )
             # Format the substituted derivative as a string in LaTeX format
             derivative_latex = sym.latex(derivative_sub)
-            derivative_str = f"{values['delta_'+variable_name][0]} \\cdot \\left|{derivative_latex} {values['delta_'+variable_name][1]}\\right|"
+            derivative_str = f"{values['delta_'+variable_name][0]} \\cdot \\left|{derivative_latex}\\right|"
             derivatives_sub.append(derivative_str)
         return " + ".join(derivatives_sub)
 
-    def latex_output(self, values):
+    def latex_out(self, values):
         latex_str = "\\begin{align*}\n"
         latex_str += (
-            "&= \\Delta "
+            "    &= \\Delta "
             + " + \\Delta ".join(
                 [
                     f"{delta_name} \\cdot \\left|{sym.latex(derivative)}\\right|"
@@ -53,10 +52,15 @@ class SymbolicDerivatives:
         for value in values.values():
             if value[0] == 0.0:
                 continue
-            substituted_str = substituted_str.replace(
-                f"{value[0]}", "\\SI{" + f"{value[0]}" + "}{" + f"{value[1]}" + "}"
-            )
-        latex_str += f"&= {substituted_str}\\\\\n"
+            if int(value[0]) == float(value[0]):
+                substituted_str = substituted_str.replace(
+                    f"{value[0]}", "\\SI{" + f"{int(value[0])}" + "}{" + f"{value[1]}" + "}"
+                )
+            else:
+                substituted_str = substituted_str.replace(
+                    f"{value[0]}", "\\SI{" + f"{float(value[0])}" + "}{" + f"{value[1]}" + "}"
+                )
+        latex_str += f"    &= {substituted_str}\\\\\n"
         # Calculate the result using the substituted values
         result = sum(
             [
@@ -65,7 +69,7 @@ class SymbolicDerivatives:
                 for delta_name in self.error_symbols
             ]
         )
-        latex_str += f"&= \\SI{{{result:.6f}}}{{}}\n"
+        latex_str += f"    &= \\SI{{{float(result)}}}{{}}\n"
         latex_str += "\\end{align*}\n\n"
         return latex_str
     def result(self, values):
@@ -88,36 +92,3 @@ class SymbolicDerivatives:
 
     def __repr__(self):
         return self.__str__()
-
-a = [38.4,38.55,38.6,38.7,38.7,38.85,39.1,39.15,39.5,39.75,39.85]
-b = [47.6,48.0,48.1,48.35,48.35,48.65,49.3,49.45,50.45,51.25,51.54]
-values2 = {
-    "a": (1.2645,''),
-    "b": (60.1,''),
-    "c": (0.03,''),
-    "l": (600,''),
-    "delta_a": (0.05,''),
-    "delta_b": (0.06,''),
-    "delta_l": (0.1,''),
-    "delta_c": (0.0005,'')
-}
-
-def my_function1(d, phi): return sym.sin((d+phi)/2)/sym.sin(phi/2)
-
-def avermoegen(a, b, c, l): return (c*a*b*l)/((l**2-b**2)*sym.sqrt((a*l**2)/(l**2-b)+1))
-
-
-
-for x in b:
-    values1 = {
-        "d": (np.radians(x), '\\degree'),
-        "phi": (np.radians(60.05), '\\degree'),
-        "delta_d": (np.radians(0.05), '\\degree'),
-        "delta_phi": (np.radians(0.05), '\\degree'),
-    }
-
-    derivatives = SymbolicDerivatives(my_function1)
-    result = derivatives.result(values1)
-    print(result)
-    latex_output = derivatives.latex_output(values1)
-    print(latex_output)
---
+++ test_error.py
@@ -0,0 +1,16 @@
+import errorcalc
+
+def my_function1(a, b):
+    return a + a * b ** 3
+
+values1 = {
+    "a": (1, '\\volt'),
+    "b": (2, ''),
+    "delta_a": (0.4, '\\volt'),
+    "delta_b": (0.5, ''),
+}
+
+derivatives = errorcalc.error(my_function1)
+error = derivatives.result(values1)
+latex_out = derivatives.latex_out(values1)
+assert error == 9.6
